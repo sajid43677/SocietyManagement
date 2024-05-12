fdd694d4fa82f883bd075e5f1facc24c530c354d
sajid43677 <20-43677-2@student.aiub.edu>
2024-05-12 13:50:18 +0600

    struture built

---
+++ dal/models/user.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal.models
+{
+    public class user
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public string password { get; set; }
+        [Required]
+        public string role { get; set; }
+        [Required]
+        public string nid { get; set; }
+    }
+}
