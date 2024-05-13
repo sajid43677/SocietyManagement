b9a56f38265c2d363de363f91da8b9476b1e4906
sajid43677 <20-43677-2@student.aiub.edu>
2024-05-14 01:14:40 +0600

    manager's services added

---
+++ bll/services/manServiceService.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using bll.DTOs;
+using dal.models;
+using dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bll.services
+{
+    public class manServiceService
+    {
+        public static List<manServiceDTO> Get()
+        {
+            var data = manDataAccessFactory.serviceData().Read();
+
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<service, manServiceDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<manServiceDTO>>(data);
+            return mapped;
+        }
+
+        public static manServiceDTO Get(int id)
+        {
+            var data = manDataAccessFactory.serviceData().Read(id);
+
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<service, manServiceDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<manServiceDTO>(data);
+            return mapped;
+        }
+
+        public static manServiceDTO AddService(manServiceDTO Service)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<manServiceDTO, service>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<service>(Service);
+            var data = manDataAccessFactory.serviceData().Create(mapped);
+            var cfg2 = new MapperConfiguration(c =>
+            {
+                c.CreateMap<service, manServiceDTO>();
+            });
+            var mapper2 = new Mapper(cfg2);
+            var mapped2 = mapper2.Map<manServiceDTO>(data);
+            return mapped2;
+        }
+
+        public static manServiceDTO UpdateService(manServiceDTO Service)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<manServiceDTO, service>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<service>(Service);
+            var data = manDataAccessFactory.serviceData().Update(mapped);
+            var cfg2 = new MapperConfiguration(c =>
+            {
+                c.CreateMap<service, manServiceDTO>();
+            });
+            var mapper2 = new Mapper(cfg2);
+            var mapped2 = mapper2.Map<manServiceDTO>(data);
+            return mapped2;
+        }
+
+        public static void DeleteService(int id)
+        {
+            manDataAccessFactory.serviceData().Delete(id);
+        }
+    }
+}
---
+++ bll/services/manTenantService.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using bll.DTOs;
+using dal;
+using dal.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bll.services
+{
+    public class manTenantService
+    {
+        public static List<manTenantDTO> Get()
+        {
+            var data = manDataAccessFactory.tenantData().Read();
+
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<tenant, manTenantDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<manTenantDTO>>(data);
+            return mapped;
+        }
+
+        public static manTenantDTO Get(int id)
+        {
+            var data = manDataAccessFactory.tenantData().Read(id);
+
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<tenant, manTenantDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<manTenantDTO>(data);
+            return mapped;
+        }
+
+        public static manTenantDTO AddTenant(manTenantDTO Tenant)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<manTenantDTO, tenant>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<tenant>(Tenant);
+            var data = manDataAccessFactory.tenantData().Create(mapped);
+            var cfg2 = new MapperConfiguration(c =>
+            {
+                c.CreateMap<tenant, manTenantDTO>();
+            });
+            var mapper2 = new Mapper(cfg2);
+            var mapped2 = mapper2.Map<manTenantDTO>(data);
+            return mapped2;
+        }
+
+        public static manTenantDTO UpdateTenant(manTenantDTO Tenant)
+        {
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<manTenantDTO, tenant>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<tenant>(Tenant);
+            var data = manDataAccessFactory.tenantData().Update(mapped);
+            var cfg2 = new MapperConfiguration(c =>
+            {
+                c.CreateMap<tenant, manTenantDTO>();
+            });
+            var mapper2 = new Mapper(cfg2);
+            var mapped2 = mapper2.Map<manTenantDTO>(data);
+            return mapped2;
+        }
+
+        public static void DeleteTenant(int id)
+        {
+            manDataAccessFactory.tenantData().Delete(id);
+        }
+    }
+}
