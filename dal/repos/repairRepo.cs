9604dbe0e40ddf001d2aaf913bdddb66d67ab652
sajid43677 <20-43677-2@student.aiub.edu>
2024-05-14 00:41:47 +0600

    repos and manager dataaccessfactory added

---
+++ dal/repos/repairRepo.cs
@@ -0,0 +1,47 @@
+﻿using dal.interfaces;
+using dal.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal.repos
+{
+    internal class repairRepo : repo, IRepo<repair, int, repair>
+    {
+        public repair Create(repair obj)
+        {
+            db.repairs.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            else return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var ex = Read(id);
+            db.repairs.Remove(ex);
+            if (db.SaveChanges() > 0) return true;
+            else { return false; }
+        }
+
+        public List<repair> Read()
+        {
+            return db.repairs.ToList();
+        }
+
+        public repair Read(int id)
+        {
+            return db.repairs.Find(id);
+        }
+
+        public repair Update(repair obj)
+        {
+            var ex = db.repairs.Find(obj.Id);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            else return null;
+
+        }
+    }
+}
---
+++ dal/repos/serviceRepo.cs
@@ -0,0 +1,47 @@
+﻿using dal.interfaces;
+using dal.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dal.repos
+{
+    internal class serviceRepo : repo, IRepo<service, int, service>
+    {
+        public service Create(service obj)
+        {
+            db.services.Add(obj);
+            if (db.SaveChanges() > 0) return obj;
+            else return null;
+        }
+
+        public bool Delete(int id)
+        {
+            var ex = Read(id);
+            db.services.Remove(ex);
+            if (db.SaveChanges() > 0) return true;
+            else { return false; }
+        }
+
+        public List<service> Read()
+        {
+            return db.services.ToList();
+        }
+
+        public service Read(int id)
+        {
+            return db.services.Find(id);
+        }
+
+        public service Update(service obj)
+        {
+            var ex = db.services.Find(obj.Id);
+            db.Entry(ex).CurrentValues.SetValues(obj);
+            if (db.SaveChanges() > 0) return obj;
+            else return null;
+
+        }
+    }
+}
